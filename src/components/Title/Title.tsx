a3fb1550d74caa59a7691dfd9fcb9cbe8f67e1c8
Pedro Augusto <pedroaugustosilva.880@gmail.com>
2022-09-25 18:36:17 -0300

    Update Components

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -3,8 +3,10 @@ import '../../Global.scss'
 import bg from '../../assets/bg-img.jpg'
 
 export const Title = () =>{
+ 
+
     return(
-        <div className="title">
+        <div className="title" >
             <h1>SHOES</h1>
             <figure>
                 <img src={bg} alt="bg-img" />
